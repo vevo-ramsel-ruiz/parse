498344246575c312b0a83b167bd8dc9972a68ac1
ramsel <ramsel.ruiz@vevo.com>
2015-06-11 11:15:02 -0700

    importPlaylist - looped httpRequests for each Sheet

--- CloudCode/cloud/main.js
+++ CloudCode/cloud/main.js
@@ -18,86 +18,88 @@ Parse.Cloud.job("importPlaylists", function(request, status) {
 		"pl" : "oi0umg5"
 	}
 
-	var countryCode = "us";
-	var sheetID = territories[countryCode];
+  	// Array to store new objects
+	var playlistsToSave = [];
 
-	var url = "https://spreadsheets.google.com/feeds/list/" + spreadsheetID + "/" + sheetID + "/public/values?alt=json"; // Make sure it is public or set to Anyone with link can view 
+	// Iterate the countries (Sheets)
+	for (var j = 0; j < territories.length; j++) {
 
-
-
-
-	// Cloud function
-	Parse.Cloud.httpRequest({
-	  url: url
-	}).then(function(httpResponse) {
-	  
-	  // Parse response
-	  var json = JSON.parse(httpResponse.buffer);
-
-	  // Array to store new objects
-	  var playlistsToSave = [];
-
-	  // Create playlist objects
-	  for (var i = 0; i < json.feed.entry.length; i++) {
-
-	  	var each = json.feed.entry[i];
-    
-    	// TODO: This needs error handling - if "gsx$" doesn't exist will crash
-      	var name = each["gsx$name"]["$t"];
-      	var playlistid = each["gsx$playlistid"]["$t"];
-      	var description = each["gsx$description"]["$t"];
-      	var imageurl = each["gsx$imageurl"]["$t"];
-      	var categories = each["gsx$categories"]["$t"].split(','); // json returns a string from Sheet - split on commas
+		var countryCode = territories[j];
+		var sheetID = territories[countryCode];
+		var url = "https://spreadsheets.google.com/feeds/list/" + spreadsheetID + "/" + sheetID + "/public/values?alt=json"; // Make sure it is public or set to Anyone with link can view 
 
 
-      	// -- Logs
-        console.log('name = ' + name);
-        console.log('playlistid = ' + playlistid);
- 		console.log('description = ' + description);
-        console.log('imageurl = ' + imageurl);
-		// console.log('categories = ' + categories);
-		// console.log('categories[1] = ' + categories[1]);
+
+		// Cloud function - http request
+		Parse.Cloud.httpRequest({
+		  url: url
+		}).then(function(httpResponse) {
+		  
+		  // Parse response
+		  var json = JSON.parse(httpResponse.buffer);
+
+		
+		  // Create playlist objects
+		  for (var i = 0; i < json.feed.entry.length; i++) {
+
+		  	var each = json.feed.entry[i];
+	    
+	    	// TODO: This needs error handling - if "gsx$" doesn't exist will crash
+	      	var name = each["gsx$name"]["$t"];
+	      	var playlistid = each["gsx$playlistid"]["$t"];
+	      	var description = each["gsx$description"]["$t"];
+	      	var imageurl = each["gsx$imageurl"]["$t"];
+	      	var categories = each["gsx$categories"]["$t"].split(','); // json returns a string from Sheet - split on commas
 
 
-		// Create Parse objects 
-		var Playlist = Parse.Object.extend("Playlist");
-		var playlist = new Playlist();
-		playlist.set("name", name);
-		playlist.set("playlistId", playlistid);
-		playlist.set("description", description);
-		playlist.set("imageUrl", imageurl);
-		playlist.set("categories", categories);
-		playlist.add("territories", countryCode);
+	      	// -- Logs
+	        console.log('name = ' + name);
+	        console.log('playlistid = ' + playlistid);
+	 		console.log('description = ' + description);
+	        console.log('imageurl = ' + imageurl);
+			// console.log('categories = ' + categories);
+			// console.log('categories[1] = ' + categories[1]);
 
 
-		// -- Logs
-		console.log(playlist.playlistid);
-		console.log(playlist.get("playlistid"));
-		// Add to caching array
-		playlistsToSave.push(playlist);
+			// Create Parse objects 
+			var Playlist = Parse.Object.extend("Playlist");
+			var playlist = new Playlist();
+			playlist.set("name", name);
+			playlist.set("playlistId", playlistid);
+			playlist.set("description", description);
+			playlist.set("imageUrl", imageurl);
+			playlist.set("categories", categories);
+			playlist.add("territories", countryCode);
 
 
-	
-      }
+			// -- Logs
+			console.log(playlist.playlistid);
+			console.log(playlist.get("playlistid"));
+			// Add to caching array
+			playlistsToSave.push(playlist);
 
 
-      // Parse - Save objects
-	  Parse.Object.saveAll(playlistsToSave, {
-        success: function(saveList) {
-            status.success("Objects created successfully.");
-        },
-        error: function(error) {
-            status.error("Unable to save objects.");
-        }
-      });
+		
+	      }
+
+		},function(httpResponse) {
+		  // error
+		  console.error('Request failed with response code ' + httpResponse.status);
+
+	      status.error("Scheduled messages error: " + error);
+		});
+	}
 
 
-	},function(httpResponse) {
-	  // error
-	  console.error('Request failed with response code ' + httpResponse.status);
-
-      status.error("Scheduled messages error: " + error);
-	});
+   	// Parse - Save objects
+	Parse.Object.saveAll(playlistsToSave, {
+	    success: function(saveList) {
+	        status.success("Objects created successfully.");
+	    },
+	    error: function(error) {
+	        status.error("Unable to save objects.");
+	    }
+	  });
 
 });
 
