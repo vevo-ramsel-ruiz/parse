7dfc6cbe07dd9e22a961896c99b78347d35e0580
ramsel <ramsel.ruiz@vevo.com>
2015-06-11 12:28:44 -0700

    importPlaylists - dependent async operations using async.js

--- CloudCode/cloud/main.js
+++ CloudCode/cloud/main.js
@@ -1,36 +1,51 @@
+
+// Modules
+var async = require("cloud/async.js");
+
+// ID of the Google Spreadsheet
+var spreadsheetId = "1dLL5tuds0n_Sg_DkEocien0FwIig15hNAwuu_f30amA";
+
+// List of country codes and their Google Sheets Ids
+var individualSheetIds = {
+	"us" : "od6",
+	"gb" : "olbnsti",
+	"it" : "oa6haa5",
+	"es" : "oxibv7k",
+	"fr" : "obisdv5",
+	"nl" : "ohrgz0b",
+	"de" : "ocqrxlj",
+	"pl" : "oi0umg5"
+}
+
+
+/** 
+ *
+ */
 Parse.Cloud.job("importPlaylists", function(request, status) {
 
 	// Use master key
 	Parse.Cloud.useMasterKey();
 
-	// ID of the Google Spreadsheet
-	var spreadsheetID = "1dLL5tuds0n_Sg_DkEocien0FwIig15hNAwuu_f30amA";
-
-	// List of country codes and their Google Sheets Ids
-	var territories = {
-		"us" : "od6",
-		"gb" : "olbnsti",
-		"it" : "oa6haa5",
-		"es" : "oxibv7k",
-		"fr" : "obisdv5",
-		"nl" : "ohrgz0b",
-		"de" : "ocqrxlj",
-		"pl" : "oi0umg5"
-	}
 
   	// Array to store new objects
 	var playlistsToSave = [];
 
-	// Iterate the countries (Sheets)
-	for (var j = 0; j < territories.length; j++) {
-
-		var countryCode = territories[j];
-		var sheetID = territories[countryCode];
-		var url = "https://spreadsheets.google.com/feeds/list/" + spreadsheetID + "/" + sheetID + "/public/values?alt=json"; // Make sure it is public or set to Anyone with link can view 
+	// // Get urls
+	// var urls = urlsForSpreadsheet();
+	
+	// console.log('urls = ' + urls);
 
 
+	async.each(individualSheetIds,
+	  // 2nd param is the function that each item is passed to
+	 function(sheetId, callback){
 
-		// Cloud function - http request
+
+
+	 	var url = urlForSheet(sheetId);
+
+	    // Call an asynchronous function, often a save() to DB
+	    // Cloud function - http request
 		Parse.Cloud.httpRequest({
 		  url: url
 		}).then(function(httpResponse) {
@@ -38,7 +53,6 @@ Parse.Cloud.job("importPlaylists", function(request, status) {
 		  // Parse response
 		  var json = JSON.parse(httpResponse.buffer);
 
-		
 		  // Create playlist objects
 		  for (var i = 0; i < json.feed.entry.length; i++) {
 
@@ -69,7 +83,9 @@ Parse.Cloud.job("importPlaylists", function(request, status) {
 			playlist.set("description", description);
 			playlist.set("imageUrl", imageurl);
 			playlist.set("categories", categories);
-			playlist.add("territories", countryCode);
+			playlist.add("territories", getKey(individualSheetIds, sheetId)); // Get the key from the sheetId
+
+
 
 
 			// -- Logs
@@ -77,30 +93,52 @@ Parse.Cloud.job("importPlaylists", function(request, status) {
 			console.log(playlist.get("playlistid"));
 			// Add to caching array
 			playlistsToSave.push(playlist);
-
-
-		
 	      }
+
+	      // Callback for 'each'
+	      callback();
+
 
 		},function(httpResponse) {
 		  // error
 		  console.error('Request failed with response code ' + httpResponse.status);
 
-	      status.error("Scheduled messages error: " + error);
+	      // status.error("Scheduled messages error: " + error);
+  	      status.error("Scheduled messages error");
 		});
-	}
 
 
-   	// Parse - Save objects
-	Parse.Object.saveAll(playlistsToSave, {
+	  },
+	  // 3rd param is the function to call when everything's done
+	  function(err){
+	   
+	   	// Parse - Save objects
+		Parse.Object.saveAll(playlistsToSave, {
 	    success: function(saveList) {
 	        status.success("Objects created successfully.");
 	    },
 	    error: function(error) {
 	        status.error("Unable to save objects.");
 	    }
-	  });
+	  	});
+	  }
+	);
+
 
 });
 
 
+function urlForSheet(sheetId) {
+
+	return "https://spreadsheets.google.com/feeds/list/" + spreadsheetId + "/" + sheetId + "/public/values?alt=json"; // Make sure it is public or set to Anyone with link can view 
+}
+
+
+function getKey(object, value) {
+  for(var key in object){
+    if(object[key] == value){
+      return key;
+    }
+  }
+  return null;
+};
